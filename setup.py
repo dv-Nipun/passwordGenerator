f0134292eb3c60de7f60104a7f2db91ea3673eb9
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-28 14:42:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="passwordP",                     # This is the name of the package
-    version="1.1.1.1",                        # The initial release version
+    version="1.1.7.7",                        # The initial release version
     author="Nipun dogra",                     # Full name of the author
     description="password generator package",
     long_description_content_type="text/markdown",
