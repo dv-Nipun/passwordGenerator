625bf2d1b676dad2766c27cdc19cc0d9fbb0fd7f
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-31 11:00:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import setuptools
 
+# Replace "1.1.7.7" with the actual version number provided as an input
+version = "1.1.7.7"
+
 setuptools.setup(
-    name="passwordP",                     # This is the name of the package
-    version="1.1.7.7",                        # The initial release version
-    author="Nipun dogra",                     # Full name of the author
-    description="password generator package",
+    name="newPassword",                      # This is the name of the package
+    version=version,                         # Version number provided via command line
+    author="Nipun Dogra",                    # Full name of the author
+    description="Password generator package",
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(),     # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
+    ],                                      # Information to filter the project on PyPI website
     python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["quicksample"],             # Name of the python package
     install_requires=[]                     # Install other dependencies if any
 )
