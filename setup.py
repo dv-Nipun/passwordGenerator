6ee586fcbd722728f8251e0d0a89f5b395ec512d
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-31 11:55:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 from version import __version__
 
 setuptools.setup(
-    name="newPassword",                      # This is the name of the package
-    version=__version__,                     # Version number from version.py
-    author="Nipun Dogra",                    # Full name of the author
+    name="newPassword",
+    version=__version__,
+    author="Nipun Dogra",
     description="Password generator package",
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),     # List of all python modules to be installed
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPI website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    install_requires=[]                     # Install other dependencies if any
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
 )
