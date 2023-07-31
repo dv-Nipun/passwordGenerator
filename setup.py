c6776b2c2c82f961949e9d1e44d28fde0bfb6e90
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-31 10:53:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,18 @@
-import sys
 import setuptools
 
-# Extract version number from command line arguments
-version = None
-for arg in sys.argv:
-    if arg.startswith('--version='):
-        version = arg.split('=')[1]
-        break
-
-if version is None:
-    raise ValueError("Version number not provided. Please use --version=xxx to specify the version.")
-
 setuptools.setup(
-    name="newPassword",                      # This is the name of the package
-    version=version,                         # Version number provided via command line
-    author="Nipun Dogra",                    # Full name of the author
-    description="Password generator package",
+    name="passwordP",                     # This is the name of the package
+    version="1.1.7.7",                        # The initial release version
+    author="Nipun dogra",                     # Full name of the author
+    description="password generator package",
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),     # List of all python modules to be installed
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPI website
+    ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["quicksample"],             # Name of the python package
     install_requires=[]                     # Install other dependencies if any
 )
