a94b07a46546443fa32ba90b3cd4bd6ecc7b83bd
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-31 11:43:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-import os
+import sys
 import setuptools
 
-# Define the version number dynamically using environment variable
-version = os.environ.get('PACKAGE_VERSION', '1.1.7.7')  # Set a default version number if not provided as input
+# Default version number
+default_version = "1.1.7.7"
+
+# Extract version number from command line arguments
+version = None
+for arg in sys.argv:
+    if arg.startswith('--version'):
+        version = arg.split('=')[1]
+        sys.argv.remove(arg)
+        break
+
+if version is None:
+    version = default_version  # Use the default version number if not provided via command line
 
 setuptools.setup(
     name="newPassword",                      # This is the name of the package
-    version=version,                         # Version number dynamically set from environment variable
+    version=version,                         # Version number provided via command line or default
     author="Nipun Dogra",                    # Full name of the author
     description="Password generator package",
     long_description_content_type="text/markdown",
